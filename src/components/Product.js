bbf865cdbad9da666d5c0037d644348ffbced27e
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-17 11:04:21 +0100

    Prettier Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,17 @@ import styled from "styled-components";
 
 export default class Product extends Component {
   render() {
-    return <ProductFrame>{this.props.product.name}</ProductFrame>;
+    return (
+      <ProductFrame>
+        <ProductImageWrapper>
+          <ProductImage src={this.props.product.img}></ProductImage>
+        </ProductImageWrapper>
+        <ProductInfoWrapper>
+          <span>{this.props.product.name}</span>
+          <span>{this.props.product.price}</span>
+        </ProductInfoWrapper>
+      </ProductFrame>
+    );
   }
 }
 
@@ -13,4 +23,24 @@ const ProductFrame = styled.div`
   min-width: 150px;
   background-color: rgb(110, 110, 110, 0.7);
   margin: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+const ProductImageWrapper = styled.div`
+  margin: 5px;
+  max-width: 150px;
+`;
+const ProductImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 25px;
+`;
+const ProductInfoWrapper = styled.div`
+  margin-top: auto;
+  margin-bottom: 5px;
+  display: flex;
+  flex-direction: column;
+  & > span {
+    text-align: center;
+  }
 `;
