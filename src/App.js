25cd8b061c6fefe2f111c1e72295f518fc1dd268
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-18 10:55:07 +0100

    Add Pages | Layout structure
    Preparing for routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
-import products from "./products.json";
-import Product from "./components/Product";
 import styled from "styled-components";
+import Header from "./components/Header";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <AppFrame className="App">
-      {products.map((product, index) => (
-        <Product product={product} key={index}></Product>
-      ))}
-    </AppFrame>
+    <>
+      <Header></Header>
+      <AppFrame className="App">
+        <Welcome></Welcome>
+        <Products></Products>
+      </AppFrame>
+    </>
   );
 }
 
 const AppFrame = styled.div`
   text-align: center;
   display: flex;
+  flex-direction: column;
 `;
 
 export default App;
